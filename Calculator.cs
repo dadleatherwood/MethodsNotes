3431eb69cfa6c8a6d1eaaebb6ba9a577eda76485
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-09-06 14:56:58 -0500

    completed notes on methods

--- Calculator.cs
+++ Calculator.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 namespace MethodsNotes
 {
-    public class Calculator
-    {
-        public Calculator()
-        {
-        }
-    }
+	public class Calculator
+	{
+		public int Add(params int[] numbers)
+		{
+			var sum = 0;
+			foreach (var number in numbers)
+			{
+				sum += number;
+			}
+			return sum;
+		}
+	}
 }
--- Program.cs
+++ Program.cs
@@ -2,11 +2,40 @@
 
 namespace MethodsNotes
 {
+    
+
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+        }
+
+        static void UseParams()
+        {
+			var calculator = new Calculator();
+			Console.WriteLine(calculator.Add(1, 2));
+			Console.WriteLine(calculator.Add(1, 2, 3));
+			Console.WriteLine(calculator.Add(1, 2, 3, 4, 5));
+			Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5, 6 }));
+        }
+
+        static void UsePoints()
+        {
+			try
+			{
+				var point = new Point(10, 20);
+				point.Move(null);
+				Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+				point.Move(100, 200);
+				Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("An unexpected error occured");
+			}
         }
     }
 }
